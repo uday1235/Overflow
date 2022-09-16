97313d0d441783758c02b6f1292cafcb4c4d0930
uday1235 <udayshankar666@gmail.com>
2022-09-16 13:05:24 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -3,9 +3,13 @@ import { getAuth } from "firebase/auth";
 import "firebase/firestore";
 import { initializeFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  // your config goes here
-  const firebaseConfig = {
-  ",
+  // your config goes hconst firebaseConfig = {
+  apiKey: "AIzaSyAIqF6N4AtElRHfEzOowIPTsIFWffue5jE",
+  authDomain: "insta-e8cac.firebaseapp.com",
+  projectId: "insta-e8cac",
+  storageBucket: "insta-e8cac.appspot.com",
+  messagingSenderId: "723254209434",
+  appId: "1:723254209434:web:dd83c53b9aec99fc8d2823",
 };
 
 export const app = initializeApp(firebaseConfig);
