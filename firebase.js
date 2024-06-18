dcdc63f37a8b2741b4f9fa1d3892c6028944a4c2
Uday <udayshankar666@gmail.com>
2024-06-18 20:25:53 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ import "firebase/firestore";
 import { initializeFirestore } from "firebase/firestore";
 const firebaseConfig = {
   // your config goes hconst firebaseConfig = {
-  apiKey: "*********HfEzOowIPTsIFW*******",
-  authDomain: "insta-e8cac.firebaseapp.com",
-  projectId: "insta-e8cac",
-  storageBucket: "insta-e8cac.appspot.com",
-  messagingSenderId: "723254209434",
-  appId: "1:723254209434:web:dd83c53b9aec99fc8d2823",
+  ",
 };
 
 export const app = initializeApp(firebaseConfig);
